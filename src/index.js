447b85ad572b31d54bf2812e9ed49f164f63d0df
KatlynCatherine <52359150+KatlynCatherine@users.noreply.github.com>
2021-01-18 03:18:14 +0100

    Update weather display and implimented sunset & sunrise

--- src/index.js
+++ src/index.js
@@ -31,7 +31,6 @@ function searchCity(event) {
 }
 
 function submitCity(city) {
-  city.preventDefault();
   let apiKey = "903072b38d3dbb124641c65ff0557cce";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(url).then(displayWeather);
@@ -47,29 +46,67 @@ search.addEventListener("submit", searchCity);
 //Display weather
 
 function displayWeather(response) {
-  let temperatureShow = document.querySelector("#temperature");
+  let temperatureShow = document.querySelector("#number");
 
   celsiusTemperature = response.data.main.temp;
 
   temperatureShow.innerHTML = Math.round(celsiusTemperature);
 
   
-  let cityShow = document.querySelector("#city");
+  let cityShow = document.querySelector("#searchedCity");
 
   cityShow.innerHTML = response.data.name;
-
 
   let descriptionShow = document.querySelector("#description");
 
   descriptionShow.innerHTML = response.data.weather[0].description;
 
+  let sunriseShow = document.querySelector("#sunrise")
+
+  sunriseShow.innerHTML = sunriseCalculation(response.data.sys.sunrise);
+
   let humidityShow = document.querySelector("#humidity");
 
   humidityShow.innerHTML = response.data.main.humidity;
 
+  let windShown = document.querySelector("#wind");
+
+  windShow.innerHTML = Math.round(response.data.wind.speed);
+
+  let iconShow = document.querySelector("#icon");
   
+  iconShow.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconShow.setAttribute("alt", response.data.weather[0].description);
+
+  let sunsetShow = querySelector("#sunrise");
+
+  sunsetShow.innerHTML = sunsetCalculation(response.data.sys.sunset);
 }
 
+//Display sunrise
+
+function sunriseCalculation(response) {
+  let a = new Date(response * 1000);
+  let hour = a.getHours();
+  let min = a.getMinutes();
+  let sec = a.getSeconds();
+  let time = hour + ':' + min;
+  return time;
+}
+
+//Display sunset
+
+function sunsetCalculation(response) {
+  let a = new Date(response * 1000);
+  let hour = a.getHours();
+  let min = a.getMinutes();
+  let sec = a.getSeconds();
+  let time = hour + ':' + min;
+  return time;
+}
 
 
 // Show current city
